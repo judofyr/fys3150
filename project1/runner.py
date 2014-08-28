3cc32faff7381e06b739a424e6e6b2d88cd1610d
Magnus Holm <judofyr@gmail.com>
2014-08-28 14:09:51 +0200

    project1: Add a Python runner.py

---
+++ project1/runner.py
@@ -0,0 +1,34 @@
+import subprocess
+import json
+import sys
+
+def run(*args):
+    sys.stderr.write("[*] Running: %s\n" % (" ".join(args)))
+    child = subprocess.Popen(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    (stdout, stderr) = child.communicate()
+    values = stdout.splitlines()
+    elapsed_start = stderr.find("elapsed: ") + 9
+    elapsed_stop = stderr.find(" ", elapsed_start)
+    elapsed = int(stderr[elapsed_start:elapsed_stop])
+    return (values, elapsed)
+
+data = []
+
+for alg in ["LU", "Tri"]:
+    for i in xrange(1, 7):
+        n = 10**i
+        if alg == "LU" and i >= 5:
+            continue
+
+        values, elapsed = run("./tridiagonal", alg, str(n))
+        data.append(
+            {
+                'algorithm': alg,
+                'n': n,
+                'values': values,
+                'elapsed': elapsed
+            }
+        )
+
+print json.dumps(data)
+
