12b1aed0876e520a907c3dbe7e506e0e049c5362
Magnus Holm <judofyr@gmail.com>
2014-08-28 14:07:57 +0200

    project1: Refactor Tridiagonal into a class; add tests

---
+++ project1/tests.cc
@@ -0,0 +1,41 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hh"
+#include "tridiagonal.hh"
+#include <cmath>
+#include <armadillo>
+
+using namespace arma;
+
+TEST_CASE("Tridiagonal poisson") {
+  int n = 100;
+  double h = 1.0 / n;
+  vec a(n); a.fill(-1);
+  vec b(n); b.fill(2);
+  vec c(n); c.fill(-1);
+
+  SECTION("f(x) = 100 * exp(-10*x)") {
+    auto f = [](double x) { return 100 * exp(-10*x); };
+    auto problem = TridiagonalProblem(n, h, f, a, b, c);
+
+    SECTION("solve() and solve_lu() should return the same value") {
+      auto v1 = problem.solve();
+      auto v2 = problem.solve_lu();
+      for (int i = 0; i < n; i++) {
+        REQUIRE(v1[i] == v2[i]);
+      }
+    }
+
+    SECTION("is kinda similar to the exact solution") {
+      auto exact = [](double x) { return 1 - (1 - exp(-10))*x - exp(-10*x); };
+
+      auto v = problem.solve();
+      double x = 0;
+      for (int i = i; i < n; i++) {
+        REQUIRE(fabs(v[i] - exact(x)) < 0.1);
+        x += h;
+      }
+    }
+  }
+}
+
---
+++ project1/tridiagonal.hh
@@ -0,0 +1,67 @@
+#include <armadillo>
+#include <chrono>
+#include <functional>
+
+class TridiagonalProblem {
+  typedef std::function<double (double)> doublef;
+
+  public:
+    int n;
+    double h;
+    arma::vec a, b, c;
+    doublef f;
+
+    TridiagonalProblem(int n, double h, doublef f, arma::vec a, arma::vec b, arma::vec c)
+      : n(n), h(h), f(f), a(a), b(b), c(c) {
+    }
+
+    arma::vec solve() {
+      arma::vec v(n);
+      double x = 0;
+
+      arma::vec bm = b;
+      arma::vec btwiddle(n);
+
+      btwiddle(0) = h*h*f(x);
+
+      for (int i = 0; i < n-1; i++) {
+        x += h;
+        double ratio = a(i+1)/bm(i);
+        bm(i+1) = bm(i+1) - ratio*c(i);
+        btwiddle(i+1) = h*h*f(x) - ratio*btwiddle(i);
+      }
+
+      // At this point we have:
+      //   b_i * v_i + c_i * v_{i+1} = btwiddle_i
+      // Rearrange this into:
+      //   v_i = (btwiddle_i - c_i * v_{i+1}) / b_i
+      // The last element on the other hand is:
+      //   v_n = btwiddle_n / b_n
+
+      v(n-1) = btwiddle(n-1) / bm(n-1);
+
+      for (int i = n-2; i >= 0; i--) {
+        v(i) = (btwiddle(i) - c(i)*v(i+1))/bm(i);
+      }
+
+      return v;
+    }
+
+    arma::vec solve_lu() {
+      arma::vec bi(n);
+      double x = 0;
+      arma::mat A(n, n); A.zeros();
+
+      for (int i = 0; i < n; i++) {
+        if (i > 0)   A(i, i-1) = a(i);
+                     A(i, i)   = b(i);
+        if (i < n-1) A(i, i+1) = c(i);
+
+        bi(i) = h*h*f(x);
+        x += h;
+      }
+
+      return arma::solve(A, bi);
+    }
+};
+
