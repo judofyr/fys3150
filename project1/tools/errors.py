30a1c5d3d710c9e0d00693206576be1b6acc10d9
Magnus Holm <judofyr@gmail.com>
2014-09-07 14:55:29 +0200

    project1: Compute errors for all N, but only plot some

--- project1/tools/errors.py
+++ project1/tools/errors.py
@@ -13,14 +13,14 @@ for run in data:
     if run['algorithm'] == 'LU':
         continue
 
-    if run['n'] > 1e5:
         continue
 
     xs = linspace(0, 1, run['n']+2)[1:-1]
     us = exact(xs)
     vs = run['values'][1:-1]
     eps = ma.log10(abs((vs-us)/us))
-    plot(xs, eps, label="N=%d" % run['n'])
+    if run['n'] < 1e5:
+        plot(xs, eps, label="N=%d" % run['n'])
     maxs[run['n']] = nanmax(eps)
 
 legend()
