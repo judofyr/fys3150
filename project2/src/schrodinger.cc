9579e11b1d561d65db7abe67f1bfa025c83b9168
Magnus Holm <judofyr@gmail.com>
2014-09-11 15:01:48 +0200

    project2: Implement first solver for Schrodinger's

---
+++ project2/src/schrodinger.cc
@@ -0,0 +1,40 @@
+#include "jacobi.hh"
+#include <armadillo>
+#include <algorithm>
+
+using namespace arma;
+
+int main() {
+  double rho_min = 0;
+  double rho_max = 5;
+  int n_step = 280;
+  double h = (rho_max - rho_min)/n_step;
+
+  int n = n_step - 1;
+
+  mat A; A.zeros(n, n);
+
+  auto diag = A.diag();
+  auto diag_upper = A.diag(+1);
+  auto diag_lower = A.diag(-1);
+
+  for (int i = 0; i < n; i++) {
+    double rho = rho_min + (i+1)*h;
+    diag(i) = 2/(h*h) + rho*rho;
+    if (i < n-1) {
+      diag_upper(i) = diag_lower(i) = -1/(h*h);
+    }
+  }
+
+  auto solver = JacobiEigenvalue(A);
+  int limit = 1e8;
+  auto lambda = solver.solve(1e-8, &limit);
+  std::sort(lambda.begin(), lambda.end());
+
+  for (auto num : lambda) {
+    std::cout << num << std::endl;
+  }
+
+  return 0;
+}
+
