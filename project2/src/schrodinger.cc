d3fe7126b3a0edfb83e949647d4710fcb5c8e653
Magnus Holm <judofyr@gmail.com>
2014-09-30 23:00:59 +0200

    project2: Print all data in schrodinger.cc

--- project2/src/schrodinger.cc
+++ project2/src/schrodinger.cc
@@ -2,18 +2,17 @@
 #include <armadillo>
 #include <algorithm>
 #include <cstdlib>
+#include <map>
 
 using namespace arma;
 
 int main(int argc, char* argv[]) {
-  if (argc < 4) {
+  if (argc < 3) {
     std::cout << "usage: " << argv[0]
-      << " n_step rho_max (val|vec) [w_r]" << std::endl
+      << " n_step rho_max [w_r]" << std::endl
       << std::endl
       << "Parameters:" << std::endl
       << "  n_step/rho_max/w_r: parameters as specified for the problem" << std::endl
-      << "  val:  prints the (sorted) list of eigenvalues" << std::endl
-      << "  vec:  prints the eigenvector for the lowest eigenvalue" << std::endl
       << std::endl
       << "When w_r is given, it will compute the wave function for two" << std::endl
       << "electrons interecting via a repulsive Coulomb interaction." << std::endl
@@ -24,8 +23,7 @@ int main(int argc, char* argv[]) {
   double rho_min = 0;
   int n_step = atoi(argv[1]);
   double rho_max = atof(argv[2]);
-  bool print_values = (argv[3][1] == 'a');
-  float w_r = (argc < 5) ? -1 : atof(argv[4]);
+  float w_r = (argc < 4) ? -1 : atof(argv[3]);
 
   double h = (rho_max - rho_min)/n_step;
 
@@ -53,21 +51,16 @@ int main(int argc, char* argv[]) {
   auto solver = JacobiEigenvalue(A);
   int limit = 1e8;
 
-  auto lambda = solver.solve(1e-8, &limit);
-  vec output;
+  auto eigval = solver.solve(1e-8, &limit);
   std::cerr << "iterations=" << (1e8 - limit) << std::endl;
 
-  if (print_values) {
-    output = lambda;
-    std::sort(output.begin(), output.end());
-  } else {
-    auto min_lambda = std::min_element(lambda.begin(), lambda.end());
-    auto min_i = min_lambda - lambda.begin();
-    output = solver.R.col(min_i);
-  }
-
-  for (auto num : output) {
-    std::cout << num << std::endl;
+  for (int i = 0; i < n; i++) {
+    std::cout << eigval(i) << " ";
+    vec eigvec = solver.R.col(i);
+    for (auto num : eigvec) {
+      std::cout << num << " ";
+    }
+    std::cout << std::endl;
   }
 
   return 0;
